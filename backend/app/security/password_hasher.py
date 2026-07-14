012ea5e66021bf10311a179e67bd95d42e194df9
Vishaldave45 <vdave8633@gmail.com>
2026-07-14 18:32:32 +0530

    feat(security): add Argon2 password hashing service

---
+++ backend/app/security/password_hasher.py
@@ -0,0 +1,12 @@
+from pwdlib import PasswordHash
+
+class PasswordHasher:
+    _password_hash=PasswordHash.recommended
+    
+    @classmethod
+    def hash(cls,password:str)->str:
+        return cls._password_hash.hash(password)
+    
+    @classmethod
+    def verify(cls,password: str, password_hash: str,) -> bool:
+        return cls._password_hash.verify(password,password_hash,)
